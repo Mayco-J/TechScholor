49e3f68988fa48f0ce4f6deb52575911289a15c6
Mayco-J <Mayco-J@users.noreply.github.com>
2020-02-03 20:25:03 -0500

    Update Tech_Scholar.java
    little cleaning

--- Tech_Scholar/src/tech_scholar/Tech_Scholar.java
+++ Tech_Scholar/src/tech_scholar/Tech_Scholar.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tech_scholar;
 
 import javafx.application.Application;
@@ -10,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-
 /**
  *
  * @author tenzintashi
@@ -28,15 +21,8 @@ public class Tech_Scholar extends Application {
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
         
-    }
-   
-    
-    
-    
+    }  
 }
